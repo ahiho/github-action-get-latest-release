23906bbbd9640445ca28674aa6fed21f7ff0f00b
Apple <apple@macbook.local>
2024-07-06 13:39:44 +0700

    feat: update nodejs runtime version 16 -> 20

--- main.js
+++ main.js
@@ -1,53 +1,52 @@
 const core = require('@actions/core');
-const { Octokit } = require("@octokit/rest");
+const { Octokit } = require('@octokit/rest');
 
 const repository = core.getInput('repository');
 const token = core.getInput('token');
 var owner = core.getInput('owner');
 var repo = core.getInput('repo');
-var excludes = core.getInput('excludes').trim().split(",");
-var replacements = core.getInput('replacements').trim().split(",");
+var excludes = core.getInput('excludes').trim().split(',');
+var replacements = core.getInput('replacements').trim().split(',');
 
 const octokit = (() => {
   if (token) {
-    return new Octokit({ auth: token,});
+    return new Octokit({ auth: token });
   } else {
     return new Octokit();
   }
 })();
 
 async function run() {
-    try {
-        if (repository){
-                [owner, repo] = repository.split("/");
-        }
-        var releases  = await octokit.repos.listReleases({
-            owner: owner,
-            repo: repo,
-            });
-        releases = releases.data;
-        if (excludes.includes('prerelease')) {
-            releases = releases.filter(x => x.prerelease != true);
-        }
-        if (excludes.includes('draft')) {
-            releases = releases.filter(x => x.draft != true);
-        }
-        if (releases.length) {
-            if (replacements.length) {
-                replacements.forEach(function(replacement) {
-                    releases[0].tag_name = releases[0].tag_name.replace(replacement, '');
-                });
-            }
-            core.setOutput('release', releases[0].tag_name);
-            core.setOutput('id', String(releases[0].id));
-            core.setOutput('description', String(releases[0].body));
-        } else {
-            core.setFailed("No valid releases");
-        }
+  try {
+    if (repository) {
+      [owner, repo] = repository.split('/');
     }
-    catch (error) {
-        core.setFailed(error.message);
+    var releases = await octokit.repos.listReleases({
+      owner: owner,
+      repo: repo,
+    });
+    releases = releases.data;
+    if (excludes.includes('prerelease')) {
+      releases = releases.filter((x) => x.prerelease != true);
     }
+    if (excludes.includes('draft')) {
+      releases = releases.filter((x) => x.draft != true);
+    }
+    if (releases.length) {
+      if (replacements.length) {
+        replacements.forEach(function (replacement) {
+          releases[0].tag_name = releases[0].tag_name.replace(replacement, '');
+        });
+      }
+      core.setOutput('release', releases[0].tag_name);
+      core.setOutput('id', String(releases[0].id));
+      core.setOutput('description', String(releases[0].body));
+    } else {
+      core.setFailed('No valid releases');
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-run()
+run();
